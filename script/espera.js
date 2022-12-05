02e2a4886292e89412137468f91c09d093a14eff
JonSilv4 <115708859+JonSilv4@users.noreply.github.com>
2022-12-04 23:46:59 -0300

    Formulário do cadastro de pedido

--- script/espera.js
+++ script/espera.js
@@ -1,6 +1,56 @@
-let cep = document.querySelector('#cep');
-let data = document.querySelector('#dataNasc')
+//CEP COM PREENCHIMENTO AUTOMÁTICO - início
 
-if(data = yyyy > 2004 && SubmitEvent) {
-    alert("Você é menor de idade.")
+let cep = document.querySelector('#cep');
+let endereco = document.querySelector('#endereco');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado = document.querySelector('#estado');
+
+cep.value = '';
+
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value;
+    let script = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
+    document.body.appendChild(script);
+});
+
+function popularForm(resposta) {
+
+    if("erro" in resposta) {
+        alert('Seu CEP não foi encontrado. Tente novamente.');
+        return;
+    }
+
+    console.log(resposta);
+    endereco.value = resposta.logradouro;
+    bairro.value = resposta.bairro;
+    cidade.value = resposta.localidade;
+    estado.value = resposta.uf;
 }
+
+//CEP COM PREENCHIMENTO AUTOMÁTICO - fim
+
+
+//ERRO AO NÃO PREENCHER CAMPOS - início
+
+const nome = document.getElementById("nome")
+const sobrenome = document.getElementById("sobrenome")
+const data = document.getElementById("floatingInputValue")
+const form = document.getElementById("form")
+const btn = document.getElementById("btn")
+const alert = document.getElementById("alert")
+const regex = /^\w+([-+.']'\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault()
+    if (!regex.test(data.value) || sobrenome.value == "") {
+        form.classList.add("was-validated")
+    } else {
+        alert.style.display = "flex"
+        setTimeout(() => location.href = "./plataforma.html",2000)
+    }
+
+})
+
+//ERRO AO NÃO PREENCHER CAMPOS - fim
